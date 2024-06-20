bd918a768ad1ad902f2ef86e2f0500d3feeaf3cc
JOEL <joelrosasgarcia223@gmail.com>
2024-06-19 21:42:13 -0500

    cambios

---
+++ app/src/main/java/com/example/guadity_app/FourthFragment.java
@@ -0,0 +1,6 @@
+package com.example.guadity_app;
+
+import android.app.Activity;
+
+public class FourthFragment extends Activity {
+}
--- app/src/main/java/com/example/guadity_app/FourthFragment.kt
+++ app/src/main/java/com/example/guadity_app/FourthFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,9 +35,11 @@ class FourthFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fourth, container, false)
     }
+
 
     companion object {
         /**
