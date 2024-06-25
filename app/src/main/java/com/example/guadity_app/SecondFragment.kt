f727673430d15ca462f552aed53848730563fa7f
JOEL <joelrosasgarcia223@gmail.com>
2024-06-24 21:13:06 -0500

    agregado de mapas y correciones de diseño

--- app/src/main/java/com/example/guadity_app/SecondFragment.kt
+++ app/src/main/java/com/example/guadity_app/SecondFragment.kt
@@ -1,12 +1,17 @@
 package com.example.guadity_app
 
-import androidx.fragment.app.Fragment
-
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -16,19 +21,21 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class SecondFragment : Fragment() {
 
+    private lateinit var map: GoogleMap
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
+    companion object {
+        const val REQUEST_CODE_LOCATION = 100
+    }
+
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map = googleMap
+        val coordinates = LatLng(-8.094959521120115, -79.0495648431584)
+        val marker = MarkerOptions().position(coordinates).title("Universidad Privada del Norte")
+        googleMap.addMarker(marker)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(coordinates))
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15f), 2000, null)
+        enableLocation() // Asegúrate de llamar a enableLocation cuando el mapa esté listo
     }
 
     override fun onCreateView(
@@ -43,5 +50,63 @@ class SecondFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
+        requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                enableLocation()
+            } else {
+                Toast.makeText(requireContext(), "Para activar la localización ve a Ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(
+        requireContext(),
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun enableLocation() {
+        if (!::map.isInitialized) return
+        if (isLocationPermissionGranted()) {
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            map.isMyLocationEnabled = true
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Toast.makeText(requireContext(), "Por favor ingresa a Ajustes y activa los permisos", Toast.LENGTH_SHORT).show()
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!::map.isInitialized) return
+        try {
+            if (isLocationPermissionGranted()) {
+                enableLocation()
+            } else {
+                map.isMyLocationEnabled = false
+                Toast.makeText(requireContext(), "Para activar la localización ve a Ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: SecurityException) {
+            map.isMyLocationEnabled = false
+            Toast.makeText(requireContext(), "Error de permisos de localización", Toast.LENGTH_SHORT).show()
+        }
     }
 }
